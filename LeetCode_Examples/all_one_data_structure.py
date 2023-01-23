078601ebc35a9cf397da7bb09a252cdacf2b940a
onurrtosunn <onurtosunn0@gmail.com>
2023-01-23 23:05:45 +0300

    Added LeetCode Examples

---
+++ LeetCode_Examples/all_one_data_structure.py
@@ -0,0 +1,40 @@
+key_list =[]
+key_lenght = 0
+def inc(key):
+    key_list.append(key)
+    leng = len(key) +1
+    print(leng)
+    key_lenght =leng
+    return key_lenght   
+
+def dec(key):
+    length = len(key) -1
+    print(len(range(length)))
+        
+
+def getMaxKey():
+    """
+    """
+
+        
+
+def getMinKey():
+        """
+        :rtype: str
+        """
+        
+inc("hello");
+inc("hello");
+print(len(range(key_lenght)))
+#getMaxKey(); // return "hello"
+#getMinKey(); // return "hello"
+#inc("leet");
+#getMaxKey(); // return "hello"
+#getMinKey(); // return "leet"
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
